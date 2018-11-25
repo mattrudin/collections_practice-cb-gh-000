7b12b314854832dae88c7eb18c5912ac26b77c29
Matthias Rudin <matthias.rudin@live.com>
2018-11-25 18:58:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
+def sort_array_asc(array)
+  array.sort()
+end
 
+def sort_array_desc(array)
+  array.sort {|a,b| b<=>a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  [array[0], array[2], array[1]]
+end
+
+def reverse_array(array)
+  array.reverse()
+end
+
+def find_a(array)
+  array.select {|element| element.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }
+end
